9a24933824658840c6d8698aaf73499649f2e8a2
DamirLuketic <luketic.damir@gmail.com>
2017-08-21 19:41:54 +0200

    Update

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UsersTableSeeder extends Seeder
 {
@@ -16,15 +17,22 @@ class UsersTableSeeder extends Seeder
         $users =
             [
                 [
-                    'role_id' => 1,
-                    'country_id' => 53,
-                    'name' => 'Damir',
-                    'email' => 'luketic.damir@gmail.com',
-                    'password' => 'pass11',
-                    'is_confirmed' => 1
+                    'role_id'       => 1,
+                    'country_id'    => 53,
+                    'name'          => 'Damir',
+                    'email'         => 'luketic.damir@gmail.com',
+                    'password'      => 'pass11',
+                    'is_confirmed'  => 1
                 ]
             ];
 
-        DB::table('users')->insert($users);
+        $users_hash =  [];
+        foreach ($users as $user)
+        {
+            $user['password'] = Hash::make($user['password']);
+            $users_hash[] = $user;
+        }
+
+        DB::table('users')->insert($users_hash);
     }
 }
